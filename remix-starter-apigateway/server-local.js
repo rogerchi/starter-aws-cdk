fb11e8d436e836b0c5a4a83d2e614cad8c074cf0
Matt Farmer <matt.farmer@wizeline.com>
2020-11-28 22:33:10 -0800

    Update to 0.8.0

--- remix-starter-apigateway/server-local.js
+++ remix-starter-apigateway/server-local.js
@@ -1,9 +1,6 @@
-//
-// NOTE: This file will only be used for local development.
-//       "server.js" will be used on AWS Lambda
-//
 const express = require("express");
 const morgan = require("morgan");
+const session = require("express-session");
 const { createRequestHandler } = require("@remix-run/express");
 const {
   createRequestHandlerConfig,
@@ -17,7 +14,19 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.static("public"));
 
-app.get("*", createRequestHandler(createRequestHandlerConfig));
+// Sessions are optional. If you don't want them, just remove this middleware.
+// Otherwise, you should configure it with a session store other than the memory
+// store so they persist. See https://www.npmjs.com/package/express-session
+app.use(
+  session({
+    secret: "r3mixR0x",
+    resave: false,
+    saveUninitialized: true,
+    sameSite: true,
+  })
+);
+
+app.all("*", createRequestHandler(createRequestHandlerConfig));
 
 let port = process.env.PORT || 3000;
 
--- remix-starter-apigateway/server.js
+++ remix-starter-apigateway/server.js
@@ -2,7 +2,7 @@
 // NOTE: This file will be used on AWS Lambda.
 //       "server-local.js" will be used for local development
 //
-const { createRequestHandler } = require("remix-run-apigateway");
+const { createRequestHandler } = require("@m14t/remix-run-apigateway");
 const {
   createRequestHandlerConfig,
 } = require("./create-request-handler-config");
